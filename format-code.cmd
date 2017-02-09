b1c5d89333d9b427c26aeb1fae69d8753cdc114d
Wei Mao <weimao1@gmail.com>
2017-02-09 15:55:19 -0800

    Update rc file

--- format-code.cmd
+++ format-code.cmd
@@ -1,5 +1,5 @@
 @echo off
-for %%d in (findapp limit-time) do (
+for %%d in (record-usage limit-time) do (
     pushd %%d
     for /f %%f in ('dir /b *.cpp *.h') do clang-format.exe -i %%f
     popd
